06289971c1ddda77f8418c68d8faaa3cc0adfe7a
Jonas-sh10 <jhonatansh17@gmail.com>
2024-08-05 18:45:57 -0500

    Modificando url

--- server.js
+++ server.js
@@ -12,10 +12,16 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(cors());
+// Configuración de CORS
+const corsOptions = {
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000', // URL de tu frontend en producción
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
-// Middleware para analizar los datos del formulario
 app.use(express.urlencoded({ extended: false }));
 
 // Middlewares
@@ -24,11 +30,9 @@ app.use("/api/v1/movies", movieRoutes);
 
 app.use(errorHandler);
 
-connectDB().then( async () => {
+connectDB().then(async () => {
     await checkAndPopulateDB();
     app.listen(port, () => {
         console.log(`Server running on port ${port}`.magenta.underline.bold);
     });
 });
-
-
